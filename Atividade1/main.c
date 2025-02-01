f0c8dc4e00e856f689ed9f1624ddc92c830ac2cf
Davi Oliveira <davioliveira541@outlook.com>
2025-02-01 16:53:38 -0300

    Updates

--- Atividade1/main.c
+++ Atividade1/main.c
@@ -11,34 +11,38 @@ bool led_green_on = false;
 
 
 // Função de callback que será chamada repetidamente pelo temporizador
-// O tipo bool indica que a função deve retornar verdadeiro ou falso para continuar ou parar o temporizador.
 bool repeating_timer_callback(struct repeating_timer *t) {
-    // Imprime uma mensagem na saída serial indicando que 1 segundo se passou.
+    //Se for para acender o led vermelho
     if(led_red_on){
         printf("LED VERMELHO\n");
         gpio_put(LED_PIN_RED,1);
         gpio_put(LED_PIN_GREEN,0);
         gpio_put(LED_PIN_BLUE,0);
+        //Atualiza o estado dos proximos leds (azul aceso)
         led_red_on = false;
         led_green_on = false;
         led_blue_on = true;
     }
+    //Se for para acender o led azul
     else if(led_blue_on)
     {
         printf("LED AZUL\n");
         gpio_put(LED_PIN_RED,0);
         gpio_put(LED_PIN_GREEN,0);
         gpio_put(LED_PIN_BLUE,1);
+        //Atualiza o estado dos proximos leds (verde aceso)
         led_red_on = false;
         led_green_on = true;
         led_blue_on = false;
     }
+    //Se for para acender o led verde
     else if(led_green_on)
     {
         printf("LED VERDE\n");
         gpio_put(LED_PIN_RED,0);
         gpio_put(LED_PIN_GREEN,1);
         gpio_put(LED_PIN_BLUE,0);
+        //Atualiza o estado dos proximos leds (vermelho aceso)
         led_red_on = true;
         led_green_on = false;
         led_blue_on = false;
@@ -50,7 +54,7 @@ int main() {
     // Inicializa a comunicação serial, permitindo o uso de funções como printf.
     stdio_init_all();
 
-    // Inicializar o pino GPIO11
+    // Inicializar os pinos dos LEDs como saída.
     gpio_init(LED_PIN_RED);
     gpio_set_dir(LED_PIN_RED,true);
     gpio_init(LED_PIN_GREEN);
@@ -59,13 +63,12 @@ int main() {
     gpio_set_dir(LED_PIN_BLUE,true);
 
     // Declaração de uma estrutura de temporizador de repetição.
-    // Esta estrutura armazenará informações sobre o temporizador configurado.
     struct repeating_timer timer;
 
-    // Configura o temporizador para chamar a função de callback a cada 1 segundo.
+    // Configura o temporizador para chamar a função de callback a cada 3000 ms (3 segundos).
     add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
-    // Loop infinito que mantém o programa em execução.
+    // Loop infinito que mantém o programa em execução para que o temporizador possa chamar a função de callback.
     while (true) {
         sleep_ms(1000);
         printf("Um segundo se passou.\n");
--- Atividade2/main.c
+++ Atividade2/main.c
@@ -2,29 +2,39 @@
 #include "pico/stdlib.h"
 #include "pico/time.h"
 
+// Definição dos pinos dos LEDs
 #define LED_PIN_GREEN 11
 #define LED_PIN_RED 13
 #define LED_PIN_BLUE 12
+
+// Variável para indicar se o LED está ativo
 bool led_active = false;
+
+// Definição do pino do botão
 const uint BUTTON_PIN = 5;
 
+// Variável para armazenar o estado atual
 int state = 0;
 
+// Função de callback para a rotina de temporização
 int64_t routine_callback(alarm_id_t id, void *user_data) {
     switch (state) {
         case 0:
+            // Desliga o LED azul e muda para o próximo estado
             gpio_put(LED_PIN_BLUE, false);
             printf("Estado 1: LED Azul DESLIGADO\n");
             state = 1;
             add_alarm_in_ms(3000, routine_callback, NULL, false);
             break;
         case 1:
+            // Desliga o LED vermelho e muda para o próximo estado
             gpio_put(LED_PIN_RED, false);
             printf("Estado 2: LED Vermelho DESLIGADO\n");
             state = 2;
             add_alarm_in_ms(3000, routine_callback, NULL, false);
             break;
         case 2:
+            // Desliga o LED verde e reseta o estado
             gpio_put(LED_PIN_GREEN, false);
             printf("Estado 3: LED Verde DESLIGADO\n");
             led_active = false;
@@ -35,8 +45,10 @@ int64_t routine_callback(alarm_id_t id, void *user_data) {
 }
 
 int main() {
+    // Inicializa a entrada e saída padrão
     stdio_init_all();
 
+    // Inicializa os pinos dos LEDs como saída
     gpio_init(LED_PIN_BLUE);
     gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
     gpio_init(LED_PIN_RED);
@@ -44,22 +56,28 @@ int main() {
     gpio_init(LED_PIN_GREEN);
     gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
 
+    // Inicializa o pino do botão como entrada com pull-up
     gpio_init(BUTTON_PIN);
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
     gpio_pull_up(BUTTON_PIN);
 
     while (true) {
+        // Verifica se o botão foi pressionado e os LEDs não estão ativos
         if (gpio_get(BUTTON_PIN) == 0 && !led_active) {
+            // Debounce do botão
             sleep_ms(50);
             if (gpio_get(BUTTON_PIN) == 0) {
+                // Liga todos os LEDs e inicia a rotina de temporização
                 gpio_put(LED_PIN_GREEN, true);
                 gpio_put(LED_PIN_RED, true);
                 gpio_put(LED_PIN_BLUE, true);
                 printf("Botão pressionado: Todos os LEDs LIGADOS\n");
+                // Marca o LED como ativo e inicia a rotina de temporização
                 led_active = true;
                 add_alarm_in_ms(3000, routine_callback, NULL, false);
             }
         }
+        // Pequena pausa para evitar uso constante da CPU
         sleep_ms(10);
     }
     return 0;
